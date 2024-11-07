a13ac403e943059c76236dd01ce39506fcfb9885
estanislao-lexicon <estanislao.sarandon@lexutb.se>
2024-11-07 23:35:00 +0100

    homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
+import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Container, Row, Col, Card } from 'react-bootstrap';
-import './App.css';
 
 
 function App() {
